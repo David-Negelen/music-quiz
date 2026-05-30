143d9af8bd22cd10e9d2d49f53b1aa1266717b8b
David Negelen <david@negelen.de>
2026-05-30 19:44:49 +0200

    Adjust tick values in graph simulation for improved performance with larger datasets

--- app.js
+++ app.js
@@ -2121,7 +2121,7 @@ const songMap = (() => {
     canvas.height = H * dpr;
 
     buildGraph(songs);
-    const ticks = songs.length > 500 ? 200 : songs.length > 200 ? 250 : 300;
+    const ticks = songs.length > 500 ? 500 : songs.length > 200 ? 400 : 300;
     await simulate(canvas, ticks);
 
     // Fit all nodes into initial view
